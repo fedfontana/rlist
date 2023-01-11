008d1702407e479fb90aaacdfd68a68a3a9674e6
fedfontana <federico.fontana2000@gmail.com>
2023-01-11 15:20:54 +0100

    added docs for the cli

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,9 @@ mod rlist;
 mod utils;
 mod db;
 
+/// Reading list manager for the command line 
 #[derive(Parser, Debug)]
+#[command(author, version, about)]
 struct Args {
     #[command(subcommand)]
     action: Action,
@@ -18,79 +20,106 @@ struct Args {
 
 #[derive(Subcommand, Debug)]
 enum Action {
+    /// Add an entry to the reading list
     #[command(aliases=&["a", "create"])]
     Add {
+        /// The name of the entry
         name: String,
 
+        /// The content of the entry
         url: String,
 
+        /// The author of the content
         #[arg(short, long)]
         author: Option<String>,
 
+        /// Topics related to the content of the entry
         #[arg(short, long, num_args = 1..)]
         topics: Vec<String>,
     },
 
+    /// Remove an entry from the reading list
     #[command(aliases=&["rm", "r", "d", "delete"])]
     Remove {
+        /// The name of the entry you want to remove
         /// Takes precedence over --topics/-t
         name: Option<String>,
 
+        /// Remove ALL of the entries that are linked to ALL of the topics specified after this option
         #[arg(short, long, num_args = 1..)]
         topics: Option<Vec<String>>,
     },
 
+    /// Edit an entry
     #[command(aliases=&["e", "mv"])]
     Edit {
+        /// The name of the entry you want to edit
         old_name: String,
 
+        /// The new name of the entry
         new_name: Option<String>,
 
+        /// The new author of the entry
         #[arg(short, long)]
         author: Option<String>,
 
+        /// The new url of the entry
         #[arg(long)]
         url: Option<String>,
 
-        /// Takes precedence over --add-topics. `--topics a b c` is the same as `--clear-topics --add-topics a b c`
+        /// Sets the topics of the entry to this list.
+        /// Takes precedence over `--add-topics`. `--topics a b c` is the same as `--clear-topics --add-topics a b c`
         #[arg(short, long, num_args = 1..)]
         topics: Option<Vec<String>>,
 
+        /// The topics you want to add to the entry
         #[arg(long, num_args = 1..)]
         add_topics: Option<Vec<String>>,
 
+        /// If set, remove the entry from all of the topics
         #[arg(long)]
         clear_topics: bool,
 
+        /// The list of topics you want the entry to be removed from
         #[arg(long, num_args = 1..)]
         remove_topics: Option<Vec<String>>,
     },
 
+    /// Show the content of your reading list
     #[command(aliases=&["ls", "l", "q", "query", "s", "search", "find", "f"])]
     List {
+        /// A substring that the name of the entries must contain
         query: Option<String>,
 
+        /// If set, the result will also show the `added` date and the topics for each entry
         #[arg(short, long)]
         long: bool,
 
+        /// Only show topics that are in all of the topics specified in this option
         #[arg(short, long, num_args = 1..)]
         topics: Option<Vec<String>>,
 
+        /// Only show the entries that have an author name that contains this substring
         #[arg(short, long)]
         author: Option<String>,
 
+        /// Only show the entries that have urls that contain this substring
         #[arg(long)]
         url: Option<String>,
 
+        /// The attribute used to sort the entries. Options are: name, author, url, added
         #[arg(short, long)]
         sort_by: Option<OrderBy>,
 
-        #[arg(long)]
+        /// Whether to sort in ascending or descending order. Aliases: `--descending`
+        #[arg(short, long, aliases=&["descending"])]
         desc: bool,
 
+        /// Only show entries added after the datetime passed to this option
         #[arg(long)]
         from: Option<String>,
 
+        /// Only show entries added before the datetime passed to this option
         #[arg(long)]
         to: Option<String>,
     },
